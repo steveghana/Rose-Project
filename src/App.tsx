472928b3dd3ea6e1315eb0d55f4aaf167e4c9d53
steveghana <boatengstephen707@gmail.com>
2023-12-19 09:26:47 +0000

    add basic component for Root task and sub tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Button, Grid, List, TextField, Box, ListItem } from "@mui/material";
+import React, { useCallback, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const TaskTracker = () => {
+  const [inputText, setInputText] = useState("");
+  const [allTasks, setAllTasks] = useState<string[]>([]);
+  const [errors, setErrors] = useState("");
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  }, []);
+  const handleClick = () => {
+    if (inputText.trim().toLowerCase() !== "") {
+      setErrors("No data found");
+      return;
+    }
+    setAllTasks([...allTasks, inputText]);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Grid container>
+      <Grid item>
+        <TextField onChange={handleChange} value={inputText} />
+        <Tasks tasks={allTasks} />
+        <Button onClick={handleClick}>Add new Task</Button>
+      </Grid>
+    </Grid>
   );
+};
+
+interface ITasks {
+  tasks: string[];
 }
 
-export default App;
+const Tasks = ({ tasks }: ITasks) => {
+  return (
+    <List>
+      {tasks.map((item, index) => (
+        <Box key={index}>
+          <ListItem>{item}</ListItem>
+        </Box>
+      ))}
+    </List>
+  );
+};
+export default TaskTracker;
